ccbb2293a9f9e6b068636dfb45b5335f21b1fe18
vanes-s <vanessayapi366@gmail.com>
2023-08-02 13:06:30 +0200

    exercice tableau

---
+++ script.js
@@ -0,0 +1,37 @@
+// let myAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
+//   "T","U","V","W","X","Y","Z","$","*","/","-","+",];
+// function myAlphabetLength() {
+//     console.log(myAlphabet.length);
+// }
+// myAlphabetLength();
+// // 31
+// let planets = [ "terre","jupitaire","mars","lune","venus"];
+// planets.forEach((planet,index)  => {
+//     console.log(planet,index);
+// });
+
+
+// let someDataTypes = ["string", 1, true, null];
+
+// someDataTypes.forEach((someDataType , index) => {
+//     console.log(typeof someDataType,index); 
+// });
+let furnitures = ['Table', 'Chairs','Couch'];
+furnitures.forEach(furniture => {
+    console.log(furniture);   
+});
+let values1= ["Apple", 1, false, "2"];
+let values2 = [`5`, "Fries", 2 , true];
+let values3 = ["Mars", "Strawberry", 9];
+
+
+let newArrayvalues =values1.concat(values2, values3);
+newArrayvalues.forEach((newArrayvalue,index) => {console.log(newArrayvalue,index);
+    
+});
+let vanessa = newArrayvalues.filter(Number);
+
+console.log(vanessa)
+
+
+
